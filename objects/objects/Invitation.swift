0ea0bda8ae9df3523ea45a904cf5d37ac1acc9a0
Eddy <kimkyunghun3@naver.com>
2022-05-01 11:20:31 +0900

    feat: 초대장 클래스 구현

---
+++ objects/objects/Invitation.swift
@@ -0,0 +1,17 @@
+//
+//  Invitation.swift
+//  objects
+//
+//  Created by Eddy on 2022/05/01.
+//
+
+import Foundation
+
+class Invitation {
+    private let when: Date
+    
+    init(when: Date) {
+        self.when = when
+        
+    }
+}
